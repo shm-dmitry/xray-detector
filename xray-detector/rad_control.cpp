4e18c9f3ecaedce4511fa3ba5af00c3b1524ae94
dmitry <dmitry@local>
2023-01-17 17:03:21 +0300

    board production started

--- xray-detector/rad_control.cpp
+++ xray-detector/rad_control.cpp
@@ -8,7 +8,9 @@
 #define RAD_CONTROL_PIN               2
 
 #define RAD_CONTROL_STORE_POINTS      12
-#define RAD_CONTROL_STORE_PER_S       10
+#define RAD_CONTROL_MAX_SECONDS_IN_PT 30
+#define RAD_CONTROL_MIN_SECONDS_IN_PT 5
+#define RAD_CONTROL_MAX_IMPULS_PER_PT 100
 
 // RAD_CONTROL_PRIORITY_VALUE - be carefull with overflow!
 #define RAD_CONTROL_PRIORITY_VALUE    2
@@ -16,7 +18,8 @@
 #define RAD_CONTROL_MINIMPL_TO_RESULT 40
 #define RAD_CONTROL_IMPL_PER_UR       100
 
-volatile uint32_t rad_control_counters[RAD_CONTROL_STORE_POINTS] = { 0 };
+volatile uint16_t rad_control_counters[RAD_CONTROL_STORE_POINTS] = { 0 };
+volatile uint8_t  rad_control_timers[RAD_CONTROL_STORE_POINTS] = { 0 };
 
 void isr_rad_control_one_event() {
   if (rad_control_counters[0] == 0 && !isrcall_uv_control_is_initialized()) {
@@ -35,23 +38,27 @@ void rad_control_init() {
 }
 
 void isrcall_rad_control_on_timer(uint8_t seconds) {
-  if (seconds % RAD_CONTROL_STORE_PER_S == 0) {
+  rad_control_timers[0]++;
+  
+  if (rad_control_timers[0] > RAD_CONTROL_MAX_SECONDS_IN_PT || 
+      (
+          rad_control_counters[0] > RAD_CONTROL_MAX_IMPULS_PER_PT && 
+          rad_control_timers[0] > RAD_CONTROL_MIN_SECONDS_IN_PT
+      )
+     ) {
     for (uint8_t i = RAD_CONTROL_STORE_POINTS - 1; i>0; i--) {
       rad_control_counters[i] = rad_control_counters[i - 1];
+      rad_control_timers[i]   = rad_control_timers[i - 1];
     }
     rad_control_counters[0] = 0;
+    rad_control_timers[0] = 0;
   }
 }
 
-bool isrcall_rad_control_calc_impl(uint32_t & impulses, uint8_t & pertime, bool fast) {
+bool isrcall_rad_control_calc_impl(uint32_t & impulses, uint16_t & pertime, bool fast) {
   for (uint8_t i = 0; i<RAD_CONTROL_STORE_POINTS; i++) {
     impulses += rad_control_counters[i];
-    if (i == 0) {
-      uint8_t secs = clock_get_time(CLOCK_TIME_SECOND);
-      pertime += secs - ((secs / RAD_CONTROL_STORE_PER_S) * RAD_CONTROL_STORE_PER_S);
-    } else {
-      pertime += RAD_CONTROL_STORE_PER_S;
-    }
+    pertime  += rad_control_timers[i];
 
     if (impulses >= RAD_CONTROL_MINIMPL_TO_RESULT) {
       return true;
@@ -70,7 +77,7 @@ bool isrcall_rad_control_calc_impl(uint32_t & impulses, uint8_t & pertime, bool 
   return false;
 }
 
-bool rad_control_calc_impl(uint32_t & impulses, uint8_t & pertime, bool isrcall = false) {
+bool rad_control_calc_impl(uint32_t & impulses, uint16_t & pertime, bool isrcall = false) {
   if (isrcall) {
     uint8_t oldSREG = SREG;
     cli();
@@ -84,7 +91,7 @@ bool rad_control_calc_impl(uint32_t & impulses, uint8_t & pertime, bool isrcall 
 
 uint32_t rad_control_dose(bool isrcall = false) {
   uint32_t impulses = 0;
-  uint8_t  time = 0;
+  uint16_t  time = 0;
 
   if (!rad_control_calc_impl(impulses, time, isrcall)) {
     return 0; // undef
